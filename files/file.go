b61ec3ca243c952e7bf61d5a57d16d2cfffaddf8
huih <1173811163@qq.com>
2016-01-28 21:45:36 +0800

    add file.go

---
+++ files/file.go
@@ -0,0 +1,36 @@
+package file
+import (
+	"strings"
+	"io/ioutil"
+	"os"
+)
+
+func WriteFile(fileName string, data []byte) error {	
+	f, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR |os.O_APPEND, os.ModePerm|os.ModeTemporary)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	
+	_, err = f.Write(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ReadFile(fileName string) ([]string, error) {
+	var dataList []string
+	f, err := os.OpenFile(fileName, os.O_RDONLY, os.ModePerm|os.ModeTemporary)
+	if err != nil {
+		return dataList, err
+	}
+	defer f.Close()
+	buf, err := ioutil.ReadAll(f)
+	if err != nil {
+		return dataList, err
+	}
+	datastr := string(buf)
+	dataList = strings.Split(datastr, "\n")
+	return dataList, nil
+}
